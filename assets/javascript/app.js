9833618bf0c241199c696383b50fce673992c692
Sally Bradfield <salzeo@gmail.com>
2017-06-03 19:22:23 -0600

    3

---
+++ assets/javascript/app.js
@@ -0,0 +1,186 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+ console.log("I'm ready")
+var timerEl=$("#timer");
+ // Create variable
+
+  var questions = [{
+    question: "Which of the below is a clarity grading of a diamond?",
+    choices: ["Dirty", "Clean", "IF3", "SI2"],
+    correctAnswer: "SI2"
+}, {
+    question: "Select which of these is a technical color grading of a diamond.",
+    choices: ["White", "Yellow", "G", "CZ"],
+    correctAnswer: "G"
+}, {
+    question: "Which is a cut grading of a diamond?",
+    choices: ["Ideal", "Better", "Square", "Round"],
+    correctAnswer: "Ideal"
+}, { 
+    question: "Which institute provides diamond grading certificates?",
+    choices: ["EGL", "ABC", "GIG", "GLD"],
+    correctAnswer: "EGL"
+}];
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+      console.log("I'm the timer")
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+           display.text(minutes + ":" + seconds);
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+  startTimer(120, timerEl)
+  // var questionCounter = 0; //Tracks question number
+  // var selections = []; //Array containing user choices
+  // var quiz = $('#quiz'); //Quiz div object
+  
+  // // Display initial question
+  // displayNext();
+  
+  // // Click handler for the 'next' button
+  // $('#next').on('click', function (e) {
+  //   e.preventDefault();
+    
+  //   // Suspend click listener during fade animation
+  //   if(quiz.is(':animated')) {        
+  //     return false;
+  //   }
+  //   choose();
+    
+  //   // If no user selection, progress is stopped
+  //   if (isNaN(selections[questionCounter])) {
+  //     alert('Please make a selection!');
+  //   } else {
+  //     questionCounter++;
+  //     displayNext();
+  //   }
+  // });
+  
+  // // Click handler for the 'prev' button
+  // $('#prev').on('click', function (e) {
+  //   e.preventDefault();
+    
+  //   if(quiz.is(':animated')) {
+  //     return false;
+  //   }
+  //   choose();
+  //   questionCounter--;
+  //   displayNext();
+  // });
+  
+  // // Click handler for the 'Start Over' button
+  // $('#start').on('click', function (e) {
+  //   e.preventDefault();
+    
+  //   if(quiz.is(':animated')) {
+  //     return false;
+  //   }
+  //   questionCounter = 0;
+  //   selections = [];
+  //   displayNext();
+  //   $('#start').hide();
+  // });
+  
+  // // Animates buttons on hover
+  // $('.button').on('mouseenter', function () {
+  //   $(this).addClass('active');
+  // });
+  // $('.button').on('mouseleave', function () {
+  //   $(this).removeClass('active');
+  // });
+  
+  // // Creates and returns the div that contains the questions and 
+  // // the answer selections
+  // function createQuestionElement(index) {
+  //   var qElement = $('<div>', {
+  //     id: 'question'
+  //   });
+    
+  //   var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+  //   qElement.append(header);
+    
+  //   var question = $('<p>').append(questions[index].question);
+  //   qElement.append(question);
+    
+  //   var radioButtons = createRadios(index);
+  //   qElement.append(radioButtons);
+    
+  //   return qElement;
+  // }
+  
+  // // Creates a list of the answer choices as radio inputs
+  // function createRadios(index) {
+  //   var radioList = $('<ul>');
+  //   var item;
+  //   var input = '';
+  //   for (var i = 0; i < questions[index].choices.length; i++) {
+  //     item = $('<li>');
+  //     input = '<input type="radio" name="answer" value=' + i + ' />';
+  //     input += questions[index].choices[i];
+  //     item.append(input);
+  //     radioList.append(item);
+  //   }
+  //   return radioList;
+  // }
+  
+  // // Reads the user selection and pushes the value to an array
+  // function choose() {
+  //   selections[questionCounter] = +$('input[name="answer"]:checked').val();
+  // }
+  
+  // // Displays next requested element
+  // function displayNext() {
+  //   quiz.fadeOut(function() {
+  //     $('#question').remove();
+      
+  //     if(questionCounter < questions.length){
+  //       var nextQuestion = createQuestionElement(questionCounter);
+  //       quiz.append(nextQuestion).fadeIn();
+  //       if (!(isNaN(selections[questionCounter]))) {
+  //         $('input[value='+selections[questionCounter]+']').prop('checked', true);
+  //       }
+        
+  //       // Controls display of 'prev' button
+  //       if(questionCounter === 1){
+  //         $('#prev').show();
+  //       } else if(questionCounter === 0){
+          
+  //         $('#prev').hide();
+  //         $('#next').show();
+  //       }
+  //     }else {
+  //       var scoreElem = displayScore();
+  //       quiz.append(scoreElem).fadeIn();
+  //       $('#next').hide();
+  //       $('#prev').hide();
+  //       $('#start').show();
+  //     }
+  //   });
+  // }
+  
+  // // Computes score and returns a paragraph element to be displayed
+  // function displayScore() {
+  //   var score = $('<p>',{id: 'question'});
+    
+  //   var numCorrect = 0;
+  //   for (var i = 0; i < selections.length; i++) {
+  //     if (selections[i] === questions[i].correctAnswer) {
+  //       numCorrect++;
+  //     }
+  //   }
+    
+  //   score.append('You got ' + numCorrect + ' questions out of ' +
+  //                questions.length + ' right!!!');
+  //   return score;
+  // }
+})
